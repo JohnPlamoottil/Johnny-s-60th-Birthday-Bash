f5dd3b1cd0e603d3cc69d5bc972c949e117edc50
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-11-08 10:19:34 -0600

    update 60birthday old changes into GitHub

--- application.js
+++ application.js
@@ -1,16 +1,14 @@
 let poll = {
-    question: "RSVP?"
-    answers:[
-        "Attending", "Maybe", "Not Attending"
-    ]
-    pollCount:20,
-    answersweight:[4, 4, 2, 10]
-    selectedAnswer:-1
+  question: "RSVP?",
+  answers: ["Attending", "Maybe", "Not Attending"],
+  pollCount: 20,
+  answersWeight: [4, 4, 2],
+  selectedAnswer: -1,
 };
 
 let pollDOM = {
-    question:document.querySelector(".poll .question"),
-    answers:document.querySelector(.poll.answers")
+  question: document.querySelector(".poll .question"),
+  answers: document.querySelector(".poll .answers"),
 };
 
 pollDOM.question.innerText = poll.question;
